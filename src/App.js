8ce74ab6fa73c60acb6b5714b3ae37ae97eab5c8
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-07-14 11:56:49 +0300

    Add hook to fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useHttps } from './hooks/useHttps';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const {request} = useHttps();
+
+  const onLoad = (d) => {
+    setData(d)
+  }
+
+  const onRequest = () => {
+    request().then(onLoad)
+  }
+
+  useEffect(()=>{
+    onRequest()
+  }, [])
+
   return (
     <div className="App">
+        {data.map((item, i)=>{
+          return (
+            <div key={i}>
+
+            {item.name}
+            </div>
+          )
+        })}
     </div>
   );
 }
