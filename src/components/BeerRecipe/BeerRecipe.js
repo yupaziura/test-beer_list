5e20570d4b7dbdadb18f6227d986d0748797cf60
YuliiaPaziura <yuliiapaziura@gmail.com>
2023-07-18 18:37:59 +0300

    Add styles and content to recipe

--- src/components/BeerRecipe/BeerRecipe.js
+++ src/components/BeerRecipe/BeerRecipe.js
@@ -1,5 +1,6 @@
 import { Paper, Divider } from '@mui/material';
 import Ingredient from '../Ingredient/Ingredient';
+import TipsAndUpdatesRoundedIcon from '@mui/icons-material/TipsAndUpdatesRounded';
 
 import './BeerRecipe.scss';
 
@@ -7,8 +8,10 @@ const BeerRecipe = ({item}) => {
     return (
         <div >
             <Paper sx={{p:2}} variant="outlined" className='recipe'>
+                <h1>{item.name}</h1>
+
                 <div className="recipe_volume">
-                    {`For ${item?.volume?.value} ${item?.volume.unit}`}
+                    <p>{`Volume: ${item?.volume?.value} ${item?.volume.unit}`}</p>
                 </div>
 
                 <Divider/>
@@ -17,6 +20,7 @@ const BeerRecipe = ({item}) => {
                     <div className="recipe_ingredients">
                         
                         <h4>Ingredients:</h4>
+
                         <div className="malt">
                             <h5>Malt:</h5>
                             {item?.ingredients.malt.map((item, i)=><div key={i}><Ingredient item={item}/></div>)}
@@ -34,12 +38,25 @@ const BeerRecipe = ({item}) => {
 
                 <Divider/>
 
+                <div className="beer_details">
+                    <p>{`Fermentation: ${item.method.fermentation.temp.value} degree ${item.method.fermentation.temp.unit}`}</p>
+                    {item.method.mash_temp.map((item, i)=> <p key={i}>{`Mash: ${item.temp.value} degree ${item.temp.unit}`}</p>)}
+                </div>
+
+                <Divider/>
+
                 <div className="recipe_pair">
                     Good with:
                     {item?.food_pairing.map((item, i)=> <div className='pair' key={i}>{item}</div>)}
                 </div>
+
+                <Divider/>
+
+                <div className="recipe_tips">
+                    <h4><TipsAndUpdatesRoundedIcon/> Tips</h4>
+                {item.brewers_tips}
+                </div>
             </Paper>
-            recipe
         </div>
     )
 }
